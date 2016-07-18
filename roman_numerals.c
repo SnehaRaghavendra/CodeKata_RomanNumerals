89091a418352e730196820ba83d57dd454be78f6
SnehaRaghavendra <sneha.raghavendra316@gmail.com>
2016-07-18 14:54:19 -0500

    Final code

--- roman_numerals.c
+++ roman_numerals.c
@@ -10,9 +10,22 @@ int main()
    int m = 0;
 // enter the mode of operation required
     printf("Enter the mode 0-add, 1-sub: ");
-    total_sum(str1,str2);
+    scanf("%d",&m);
+ switch (m)
+    {
+    case 0: printf("\nThe Sum of two Roman Numerals are: ");
+            total_sum(str1, str2);
+            break;
+    case 1:
+            printf("\nThe difference of two Roman Numerals are: ");
+            difference(str1, str2);
+            break;
+    default:
+            printf("\nNone selected");
+            break;
+    }
 
-    return 0;
+ return 0;
 }
 
 
