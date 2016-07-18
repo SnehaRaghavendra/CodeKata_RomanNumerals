63e9df2e996efece9acc9bf9e262f5d7485e516d
SnehaRaghavendra <sneha.raghavendra316@gmail.com>
2016-07-18 14:34:39 -0500

    generating decimal values

---
+++ roman_numerals.c
@@ -0,0 +1,16 @@
+#include "roman_header.h" // include the header file that was created
+#include "roman_1.c"    // to include the function definition file
+
+int main()
+{
+
+// initialize the characters with the test cases required
+    char str1[] ="LXII";
+    char str2[]= "C";
+printf("\n%d",num_val(str1));
+printf("\n%d\n",num_val(str2));
+
+    return 0;
+}
+
+
