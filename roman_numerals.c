eb88eb8e58a68b87d695d665d8a3da5cd8d23f91
SnehaRaghavendra <sneha.raghavendra316@gmail.com>
2016-07-18 14:41:26 -0500

    addition and subtractcion

--- roman_numerals.c
+++ roman_numerals.c
@@ -7,8 +7,8 @@ int main()
 // initialize the characters with the test cases required
     char str1[] ="LXII";
     char str2[]= "C";
-printf("\n%d",num_val(str1));
-printf("\n%d\n",num_val(str2));
+total_sum(str1,str2);
+difference(str1,str2);
 
     return 0;
 }
